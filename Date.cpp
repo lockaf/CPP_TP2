bc980d219546bd76cc8f904ec06b121a460b11af
Bumbolo <louis.bumbolo@cpe.fr>
2019-03-02 14:30:52 +0100

    Ajout constructeur et getteur

--- Date.cpp
+++ Date.cpp
@@ -5,7 +5,22 @@
 #include <string>
 using namespace std;
 
+//Constructeurs
+Date::Date()
+	:day(0),month(0),year(0) {}
 
+Date::Date(int day, int month, int year) //Faut-il mettre dautres noms? 
+	:day(this.day), month(this.month), year(this.year) {}
+
+//Getteurs
+const int& Date::day() const {return day}
+const int& Date::month() const {return month}
+const int& Date::year() const {return year}
+
+//Setteurs
+int& Date::day() {return day}
+int& Date::month() {return month}
+int& Date::year() {return year}
 
 static bool is_date(int day, int month, int year){
 	bool ret=false;
