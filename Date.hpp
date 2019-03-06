a1ef999f360c2202121b3f54caecdb8796c008e1
Bumbolo <louis.bumbolo@cpe.fr>
2019-03-06 11:15:38 +0100

    Classe Date effective, je fais le to_string la

--- Date.hpp
+++ Date.hpp
@@ -1,25 +1,33 @@
 #ifndef date
 #define date
+#include <fstream>
+#include <iostream>
+#include <string>
 
 class Date {
-    int day;int month;int year;
+    int day_value;
+    int month_value;
+    int year_value;
+
+
 
 
 public:
-    const int& day();
-    const int& month();
-    const int& year();
+    const int& day() const;
+    const int& month() const;
+    const int& year() const;
 
     int& day();
     int& month();
     int& year();
 
     Date();
-    Date(int day,int month,int year);
+    Date(int day_param,int month_param,int year_param);
 
-    static bool is_date(int day, int month, int year);
+    static bool is_date(Date const& d);
     static std::string to_string(const Date& date);
-    static bool parse_date(const std::string& date_name)
+    static Date from_string(const std::string& name);
 
-    
 };
+
+#endif
