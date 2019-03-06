4e70d13d4c5614462badc432e0d46c41a4e8fa0b
Maxime Dumont <maxime.dumont@mastertp>
2019-03-06 17:46:17 +0100

    gjjg

--- person.cpp
+++ person.cpp
@@ -6,15 +6,20 @@
 #include <string>
 
 
-//Constructeur vide
+    //Constructeur vide
 Person::Person()
       : birth(Date ()){}
 
-//Constructeur
+    //Constructeur
 Person::Person(Date birth_param)
       : birth(birth_param){}
 
-    //Methodes
+    /*Methodes*/
+
+/*Name : get_age
+ * Function : rnevoie l'age d'un personnage en fonction
+ * de son année de naissance et d ela date actuelle
+ * */
 int Person::get_age(const Date& current_date){
    int age = current_date.year() - birth.year();
             if (current_date.month() < birth.month() || (current_date.month() == (birth.month() && current_date.day() < birth.day()))){
