ef6744b3dd835b2115ed27c550f1763493af4c59
Maxime Dumont <maxime.dumont@mastertp>
2019-03-06 17:06:22 +0100

    dsqf

--- person.cpp
+++ person.cpp
@@ -1,10 +1,27 @@
 #include "person.hpp"
+#include "Date.hpp"
 
 #include <fstream>
 #include <iostream>
 #include <string>
 
-Person::Person()
-{
 
+//Constructeur vide
+Person::Person()
+      : birth(Date ()){}
+
+//Constructeur
+Person::Person(Date birth_param)
+      : birth(birth_param){}
+
+    //Methodes
+int Person::get_age(const Date& current_date){
+   int age = current_date.year() - birth.year();
+            if (current_date.month() < birth.month() || (current_date.month() == (birth.month() && current_date.day() < birth.day()))){
+                age = age-1;
+            }
+   return age;
 }
+
+
+
--- person.hpp
+++ person.hpp
@@ -1,13 +1,27 @@
 #ifndef PERSON_HPP
 #define PERSON_HPP
 
+#include "Date.hpp"
 #include <fstream>
 #include <iostream>
 #include <string>
 
 class Person{
+private:
+    Date birth;
 public:
+    //Constructeurs
     Person();
+    Person(Date birth);
+
+    //Getteur
+    const int& getteur_dob() const;
+
+    //Setteur
+    void set_dob(int new_dob);
+
+    //Methode
+    int get_age(const Date& current_date);
 };
 
 #endif // PERSON_HPP
